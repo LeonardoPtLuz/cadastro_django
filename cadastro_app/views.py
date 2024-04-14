4cfa1ce3303f0e65132b156ae172e71e66575801
java_ada <leonardoptluz@hotmail.com>
2024-04-14 16:31:33 -0300

    cadastro-web-v1.1

--- cadastro_app/views.py
+++ cadastro_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .models import User
 from .forms import UserForm
 from  django.core.paginator import Paginator
+#from django.contrib.auth.decorators import login_required
 
 
 
@@ -52,4 +53,4 @@ def user_delete(request, user_id):
     if request.method == 'POST':
         user.delete()
         return redirect('list_users')
-    return render(request, 'delete_user.html', {'user': user})
+    #return render(request, 'delete_user.html', {'user': user})
